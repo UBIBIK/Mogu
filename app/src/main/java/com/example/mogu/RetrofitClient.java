db94693ca94ae0b8ea7b7f577f1bf231014cd66d
오민성 <dhalstjd123@naver.com>
2024-07-02 23:32:25 +0900

    RetrofitClient 서버 주소 수정, AndroidManifest.xml usesCleartextTraffic 설정 추가, SignUp activity 추가

--- app/src/main/java/com/example/mogu/RetrofitClient.java
+++ app/src/main/java/com/example/mogu/RetrofitClient.java
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
     private static Retrofit retrofit;
-    private static final String BASE_URL = "http://10.0.0.1";
+    private static final String BASE_URL = "http://10.0.2.2:8080";
 
     public static Retrofit getRetrofitInstance() {
         if (retrofit == null) {
